88bba8b07fc40d0000b0d7d081f7fdfd190dc8f9
李先生 <5738344+starPlan@user.noreply.gitee.com>
2025-09-16 22:05:17 +0800

    initial commit

---
+++ LyChat/src/main/java/com/sunnyday/lychat/LyChatApplication.java
@@ -0,0 +1,13 @@
+package com.sunnyday.lychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyChatApplication.class, args);
+    }
+
+}
